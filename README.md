73c2a5ee72e208bdf9931e08c4ed65b9bc688a9b
Julian Müller <undqt@student.kit.edu>
2021-10-22 21:49:35 +0200

    Add setup.sh

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Setup Project
+
 # Folder structure
 ```
 |-- data/                   
---
+++ setup.sh
@@ -0,0 +1,8 @@
+# Create folders
+mkdir -p data/raw/
+mkdir -p data/processed/
+mkdir notebooks/
+mkdir models/
+
+# Install poetry env
+poetry install
